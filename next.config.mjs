af63e1690de7fb1516db2f268231d3bdfbd8f4f0
Toufiq <toufiq.austcse@gmail.com>
2026-05-31 00:49:28 +0600

    feat: add deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,10 @@ import withMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
